54726a8bdae28303b49462f99fd74b5cd4150ec8
krlargo <krlargo@ucdavis.edu>
2017-12-13 22:28:33 -0800

    Add header for Color enum

---
+++ Chess/Color.hpp
@@ -0,0 +1,14 @@
+//
+//  Color.hpp
+//  Chess
+//
+//  Created by Kevin Largo on 12/13/17.
+//  Copyright © 2017 xkevlar. All rights reserved.
+//
+
+#ifndef Color_hpp
+#define Color_hpp
+
+enum Color { white, black };
+
+#endif /* Color_hpp */
