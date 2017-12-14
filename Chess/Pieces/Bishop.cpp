e5287ea25367aee8232f8140dba498de1f36e605
krlargo <krlargo@ucdavis.edu>
2017-12-14 12:04:58 -0800

    Implement isValidMove()

--- Chess/Pieces/Bishop.cpp
+++ Chess/Pieces/Bishop.cpp
@@ -6,3 +6,9 @@ Bishop::Bishop(Color color, square position) : Piece(color, position) {
     letter = 'B';
 }
 
+bool Bishop::isValidMove(square destination) {
+    if(position == destination) return false;
+    int dy = abs(destination.first - position.first);
+    int dx = abs(destination.second - position.second);
+    return dx == dy;
+}
